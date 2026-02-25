940556f7b447814f74b61e119b5cad9938309555
ItsNiccy <MorningCoffee5525@protonmail.com>
2026-02-25 18:05:01 +1030

    Delete Lua Source Files/wantedevent_stategraph_basicrace_prerace_handler_stateenter.lua

--- Lua Source Files/wantedevent_stategraph_basicrace_prerace_handler_stateenter.lua
+++
@@ -1,7 +0,0 @@
-h_265AD682 = function(param0, param1, param2)
-	Music.ForcePursuitMusic()
-	Game.ForcePursuitStart(param0.ForceHeatLevel)
-	param2.PassedMilestone = false
-	param2.CareerRace = Game.IsCareerMode()
-end
-
