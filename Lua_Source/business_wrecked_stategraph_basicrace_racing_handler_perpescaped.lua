7d100375e4c6be26fe823eae5da635c5546e94cc
ItsNiccy <MorningCoffee5525@protonmail.com>
2026-03-23 22:43:46 +1030

    Add files via upload

--- Lua_Source/business_wrecked_stategraph_basicrace_racing_handler_perpescaped.lua
+++ Lua_Source/business_wrecked_stategraph_basicrace_racing_handler_perpescaped.lua
@@ -7,9 +7,9 @@ h_1210170C = function(param0, param1, param2)
         G1.SetTimer("we_music", 13)
 	end
 	if param2.PlayerHasWon and G1.RacerIsHuman(G1.GetRacerIndex(param1.Perpetrator)) then
-        RaceStatus:SetHasBeenWon(true)
-		G1.NotifyRaceFinished(param1.Perpetrator)
         G1.ChallengeComplete(param1.Perpetrator)
+		G1.NotifyRaceFinished(param1.Perpetrator)
+        RaceStatus:SetHasBeenWon(true)
 		ChangeState("raceover")
 	end
 end
