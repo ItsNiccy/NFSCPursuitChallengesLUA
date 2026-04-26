a1750c20ca8cb90b11e45a99707fbe0d4a94a860
ItsNiccy <MorningCoffee5525@protonmail.com>
2026-04-26 21:20:08 +0930

    Delete Lua_Source/wantedevent_phototicket_stategraph_basicrace_racing_handler_stateenter_italian.lua

--- Lua_Source/wantedevent_phototicket_stategraph_basicrace_racing_handler_stateenter_italian.lua
+++
@@ -1,8 +0,0 @@
-h_D326B272 = function(param0, param1, param2)
-	Debug.ShowScreenMessage("OBIETTIVO AUTOVELOX:\n"..param0.ThreshholdSpeed.."KM/H")
-    param2.PlayerHasWon = false
-	if param0.AutoActivateGPS then
-		Game.ShowGPS(true)
-		Game.NavigatePlayerTo(param0.phototickettrigger, param0.phototickettrigger, 0, true)
-	end
-end
